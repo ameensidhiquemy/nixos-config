5cebb76bfc1eb54bbf3551bbb775aa2064835f4f
ameensidhique <ameen.sidhique10@gmail.com>
2025-06-28 21:54:04 +0530

    formatted the codebase

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # NixOS official package source, using the nixos-25.05 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-        home-manager = {
+    home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
@@ -15,24 +15,34 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, plasma-manager,... }@inputs: {
-    # Please replace my-nixos with your hostname
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        ./configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.sharedModules =
-            [ plasma-manager.homeManagerModules.plasma-manager ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      plasma-manager,
+      ...
+    }@inputs:
+    {
+      # Please replace my-nixos with your hostname
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          # Import the previous configuration.nix we used,
+          # so the old configuration file still takes effect
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.sharedModules = [
+plasma-manager.homeManagerModules.plasma-manager ];
 
-          home-manager.users.ameen = { imports = [ ./home.nix ]; };
-        }
-      ];
+            home-manager.users.ameen = {
+              imports = [ ./home.nix ];
+            };
+          }
+        ];
+      };
     };
-  };
 }
