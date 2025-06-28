03c70d8570c825920ffe92fe6edc0e4778beca6c
ameensidhique <ameen.sidhique10@gmail.com>
2025-06-28 21:48:32 +0530

    added nautilus and zsh

--- home.nix
+++ home.nix
@@ -5,6 +5,55 @@
   # manage.
   home.username = "ameen";
   home.homeDirectory = "/home/ameen";
+programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+#     autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      ll = "ls -l";
+      edit = "sudo -e";
+      update = "sudo nixos-rebuild switch";
+    };
+
+#     histSize = 100000;
+#     histFile = "$HOME/.zsh_history";
+#     setOptions = [
+#       "HIST_IGNORE_ALL_DUPS"
+#     ];
+   enableAutosuggestions = true;
+
+
+
+    oh-my-zsh = { # "ohMyZsh" without Home Manager
+    enable = true;
+    plugins = [ "git" "thefuck" ];
+    theme = "robbyrussell";
+  };
+  plugins = [
+    {
+      # will source zsh-autosuggestions.plugin.zsh
+      name = "zsh-autosuggestions";
+      src = pkgs.fetchFromGitHub {
+        owner = "zsh-users";
+        repo = "zsh-autosuggestions";
+        rev = "v0.4.0";
+        sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
+      };
+    }
+    {
+      name = "enhancd";
+      file = "init.sh";
+      src = pkgs.fetchFromGitHub {
+        owner = "b4b4r07";
+        repo = "enhancd";
+        rev = "v2.2.1";
+        sha256 = "0iqa9j09fwm6nj5rpip87x3hnvbbz9w9ajgm6wkrd5fls8fn8i5g";
+      };
+    }
+  ];
+};
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -13,7 +62,7 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "25.05"; # Please read the comment before changing.
+  home.stateVersion = "25.11"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
